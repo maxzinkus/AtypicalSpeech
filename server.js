4eec8895af0332d3e291718d2f2d2cf26a31763f
forrestpark <pjwoo3@gmail.com>
2022-10-22 20:53:37 -0400

    connect server with react

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const port = process.env.PORT || 3000;
 
 console.log("server.js");
 
+app.get("/", (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
 app.listen(port, function() {
     console.log('Express listening on port', port);
 })
