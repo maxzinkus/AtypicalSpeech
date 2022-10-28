2e57d285f12efe547316950dd8bac1b10939cba5
forrestpark <pjwoo3@gmail.com>
2022-10-28 16:38:52 -0400

    add recording feature

--- client/src/components/RecordingModal.js
+++ client/src/components/RecordingModal.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ScriptController from './ScriptController'
 import { useState } from 'react';
 import UtteranceDisplayer from './UtteranceDisplayer';
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
 
 function RecordingModal() {
 
-    const [currentState, setCurrentState] = useState({currentLine: 0})
+    const [currentState, setCurrentState] = useState({currentLine: 0, recordState: null})
     const [error, setError] = useState("");
 
     const utterances = JSON.parse('["Ellen looked out at the street through the glass front.", " The man from four hundred and ten was standing out there, smoking a cigarette, watching her.", " When their eyes met, he abruptly threw away the cigarette and started walking toward the apartment house.", " Again she felt that faint dread she had experienced in the hall earlier.", " The waitress picked up her quarter, gave her back a nickel and a dime.", " four hundred and ten was just ahead of her in the lobby.", "He held the front door open for her.", " He opened the elevator doors, too, and she stepped in ahead of him.", " When the doors clanged shut, she had a feeling of panic.", "Come in, my dear, come in.", " She almost fell over the landing.", " The door closed behind her.", " She stumbled to the davenport, sank down, gasping.", " Two cats rubbed against her legs, purring.", " Two cats? She heard herself say stupidly, Missis Moffatt, wheres the other cat? and wondered why she said it."]')
@@ -30,10 +31,38 @@ function RecordingModal() {
         console.log("next line: ", currentState.currentLine)
     }
 
+    const start = (event) => {
+        console.log("Start")
+        setCurrentState({
+            recordState: RecordState.START
+        })
+    }
+
+    const pause = (event) => {
+        console.log("Pause")
+        setCurrentState({
+            recordState: RecordState.PAUSE
+        })
+    }
+
+    const stop = (event) => {
+        setCurrentState({
+            recordState: RecordState.STOP
+        })
+    }
+
+    const onStop = (audioData) => {
+        console.log('audioData', audioData)
+    }
+
+    const recordState = currentState.recordState
+
     return (
-        <><div>RecordingModal</div>
-        <UtteranceDisplayer line={utterances[currentState.currentLine]}></UtteranceDisplayer>
-        <ScriptController previousLine={previousLine} nextLine={nextLine}></ScriptController></>
+        <><div>RecordingModal</div><div>
+            <UtteranceDisplayer line={utterances[currentState.currentLine]}></UtteranceDisplayer>
+            <AudioReactRecorder state={recordState} onStop={onStop}></AudioReactRecorder>
+            <ScriptController previousLine={previousLine} nextLine={nextLine} start={start} stop={stop} pause={pause}></ScriptController>
+        </div></>
     )
 }
 
--- client/src/components/ScriptController.js
+++ client/src/components/ScriptController.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 
-const ScriptController = ({previousLine, nextLine}) => (
+const ScriptController = ({previousLine, nextLine, start, stop, pause}) => (
   <Button.Group>
     <Button labelPosition='left' icon='left chevron' content='Back' onClick={previousLine}/>
-    <Button icon='stop' content='Play' />
-    <Button icon='stop' content='Pause' />
-    <Button icon='stop' content='Stop' />
+    <Button icon='play' content='Play' onClick={start}/>
+    <Button icon='pause' content='Pause' onClick={pause}/>
+    <Button icon='stop' content='Stop' onClick={stop}/>
     <Button labelPosition='right' icon='right chevron' content='Forward' onClick={nextLine}/>
   </Button.Group>
 )
