8640751d636ea366eda331a5d92b15e3fdc87e67
forrestpark <pjwoo3@gmail.com>
2022-11-30 18:24:13 -0500

    add create script api

--- controllers/script/script.ctrl.js
+++ controllers/script/script.ctrl.js
@@ -1,5 +1,16 @@
 const {models, Script} = require('../../models')
 
+exports.create_script = async (req, res) => {
+    const {id, utterances} = req.body
+    try {
+        const script = await City.create({id, utterances})
+        return res.json(script)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err)
+    }
+}
+
 // exports.name = async (req, res) => {
 //     try {
 //     } catch (err) {
