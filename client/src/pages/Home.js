a81d3aa145d2481cff290f987d282a7de50f9607
forrestpark <pjwoo3@gmail.com>
2023-01-09 15:32:40 +0900

    added jhu som logo on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import LoginForm from '../components/LoginForm';
 import RecordingModal from '../components/RecordingModal';
 import "../../src/App.css"
 import Logo from "./../images/logo1.png"
+import SOM_Logo from "./../images/rounded_jhu_som_logo_transparent_white.png"
 
 function Home() {
 
@@ -32,6 +33,9 @@ function Home() {
       <br></br>
       <div className="center">
         <img className="logo" src={Logo} alt="logo1"></img>
+      </div>
+      <div>
+        <img className='jhu_som_logo' src={SOM_Logo} alt="Johns Hopkins School of Medicine"></img>
       </div>
       <div className='right'>
         {renderLoginForm()}
