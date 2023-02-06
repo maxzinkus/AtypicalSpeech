a838da5e09057ccbfd093a1c8548d8f75bb526a3
forrestpark <pjwoo3@gmail.com>
2023-02-06 21:08:25 +0900

    display progress update alert

--- client/src/components/SaveProgressButton.js
+++ client/src/components/SaveProgressButton.js
@@ -4,17 +4,21 @@ import axios from 'axios';
 
 function SaveProgressButton({user_id, script_id, current_line}) {
 
+    function displayProgressUpdateAlert(line_to_update) {
+        alert("Progress successfully saved at line #" + line_to_update.toString() + "!")
+    }
+
     async function saveProgress(user_id, script_id, line) {
-
-        console.log("save progress: ", line)
-
-        const update_task_progress_result = await axios.post('http://localhost:3000/user/update_task_progress', {user_id: user_id, script_id: script_id, current_line: (line >= 0) ? line : 0});
-        console.log("update_task_progress_result: ", update_task_progress_result)
-
+        const line_to_update = (line >= 0) ? line : 0;
+        await new Promise(
+            async resolve => 
+                await axios.post('http://localhost:3000/user/update_task_progress', {user_id: user_id, script_id: script_id, current_line: line_to_update})
+            )
+            .then(displayProgressUpdateAlert(line_to_update))
     }
 
   return (
-    <Button className='button_right_align' onClick={saveProgress(user_id, script_id, current_line - 1)}>
+    <Button className='button_right_align' onClick={() => saveProgress(user_id, script_id, current_line - 1)}>
         Save Progress
     </Button>
   )
