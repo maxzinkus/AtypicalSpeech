799f9e5c1e4e6b5a0bab272a606bdeea50c60585
forrestpark <pjwoo3@gmail.com>
2023-02-08 16:17:58 +0900

    added admin tab skeleton

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 function AdminDashboard() {
-  return (
-    <div>AdminDashboard</div>
-  )
+    return (
+        <Tabs
+          defaultActiveKey="profile"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey="home" title="Home">
+            #1
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+            #2
+          </Tab>
+          <Tab eventKey="contact" title="Contact" disabled>
+            #3
+          </Tab>
+        </Tabs>
+      );
 }
 
 export default AdminDashboard
