c004c8947f88bc82f89c6cccc453ed716e34323f
forrestpark <pjwoo3@gmail.com>
2023-02-08 15:13:24 +0900

    add AdminDashboard routing

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import LoginForm from './components/LoginForm'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import RecordingModal from './components/RecordingModal'
+import AdminDashboard from './components/admin/AdminDashboard'
 
 const api = axios.create({
   baseURL: `http://localhost:3001/`
@@ -23,7 +24,7 @@ function App() {
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/dashboard" element={<Dashboard />}/>
             <Route path="/module" element={<RecordingModal/>}/>
-            {/* <Route path="/" element={<RecordingModal/>}/> */}
+            <Route path="/admin" element={<AdminDashboard/>}/>
           {/* </Switch> */}
           </Routes>
         </div>
