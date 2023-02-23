54202b808205bd0aa25bd5e5585c55c0d0f99f90
forrestpark <pjwoo3@gmail.com>
2023-02-23 13:14:26 +0900

    switch inline css style to explicit for utterance display

--- client/src/components/UtteranceDisplayer.js
+++ client/src/components/UtteranceDisplayer.js
@@ -24,21 +24,12 @@ function UtteranceDisplayer({user_id, script_id, line, current_line, currentReco
     visibility_timeout();
 
   }, [current_line])
-
-  const mystyle = {
-    color: "white",
-    backgroundColor: "DodgerBlue",
-    padding: "10px",
-    fontFamily: "Arial",
-  };
   
-
   return (
-    <div style={mystyle}>
-      {/* Current utterance: {(currentRecordState === "start" || currentRecordState === "stop") ? (<a className='utterance_display_bold'>{currentState.line_to_display}</a>) : (<a className='utterance_display_bold'></a>)} */}
+    <div className='utterance_display_whole'>
       Current utterance: {currentRecordState !== null && <a id='utterence_line_display' className='utterance_display_bold'>{line}</a>}
       {SaveProgressButton({user_id: user_id, script_id: script_id, current_line: current_line})}
-      </div>
+    </div>
   )
 }
 
