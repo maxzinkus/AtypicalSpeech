10fccf29433be6c67eb01768254fbd300df437ed
forrestpark <pjwoo3@gmail.com>
2023-02-23 14:56:40 +0900

    adjusted top bottom padding for utterance display

--- client/src/components/UtteranceDisplayer.js
+++ client/src/components/UtteranceDisplayer.js
@@ -26,25 +26,20 @@ function UtteranceDisplayer({user_id, script_id, line, current_line, currentReco
   }, [current_line])
   
   return (
-    <div className='float-container'>
+    <><div className='float-container'>
       <div className='float-left-child'>
         <div>Current utterance:</div>
-        <br></br>
-        <div>{SaveProgressButton({user_id: user_id, script_id: script_id, current_line: current_line})}</div>
 
       </div>
       <div className='float-right-child'>
         {/* <a className='utterance_display_bold'> */}
-          {/* {currentRecordState !== null && {line}} */}
-          {line}
+        {/* {currentRecordState !== null && {line}} */}
+        {line}
         {/* </a> */}
       </div>
 
-    </div>
-    // <div className='utterance_display_whole'>
-    //   Current utterance: {currentRecordState !== null && <a id='utterence_line_display' className='utterance_display_bold'>{line}</a>}
-    //   {SaveProgressButton({user_id: user_id, script_id: script_id, current_line: current_line})}
-    // </div>
+    </div><div>{SaveProgressButton({ user_id: user_id, script_id: script_id, current_line: current_line })}</div></>
+
   )
 }
 
