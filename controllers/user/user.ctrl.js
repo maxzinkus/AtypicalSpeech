16066312c1407c9eb0ab2c119b9cb049b69bc511
forrestpark <pjwoo3@gmail.com>
2023-04-16 15:32:35 +0900

    clean up user.ctrl.js

--- controllers/user/user.ctrl.js
+++ controllers/user/user.ctrl.js
@@ -71,42 +71,6 @@ exports.assign_multiple_tasks = async (req, res) => {
         await user.save()
     
         return res.json(user)
-
-        // const user = await User.findByPk(user_id)
-
-        // // if user not found, show error
-        // if (user === null) {
-        //     console.log("user not found!")
-        // }
-
-        // const scripts = await Script.findAll({
-        //     where : {
-        //         id : script_ids
-        //     }
-        // })
-
-        // console.log("assign_multiple_tasks scripts: ", scripts)
-
-        // // if script not found, show error
-        // // if (script === null) {
-        // //     console.log("script not found!")
-        // // }
-
-        // const original_assigned_tasks = user.assignedTasks.tasks
-
-        // console.log("original_assigned_tasks: ", original_assigned_tasks);
-
-        // scripts.map((script_id) => {
-        //     if (!original_assigned_tasks.includes(script_id)) {
-        //         original_assigned_tasks.push(script_id)
-        //     }
-        // })
-
-        // user.assignedTasks.tasks = original_assigned_tasks
-        // user.changed('assignedTasks', true)
-        // await user.save()
-    
-        // return res.json(user)
     } catch (err) {
         console.log(err)
         return res.status(500).json(err)
