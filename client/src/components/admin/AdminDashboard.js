b229e4bdc205bd38a33edb86cca8368571cff89e
Jim <mr.totihuang@gmail.com>
2023-07-05 10:16:04 -0400

    feat: reconstruct admin dashboard

--- client/src/components/admin/AdminDashboard.js
+++ client/src/components/admin/AdminDashboard.js
@@ -1,29 +1,76 @@
-import React from 'react'
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import ScriptTab from './ScriptTab';
+import React, { useState } from 'react';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons'
+import { Layout, Menu, theme } from 'antd';
+
 import UserTab from './UserTab';
+import ScriptTab from './ScriptTab';
+import Statistic from './Statistic'
 
-function AdminDashboard() {
-    return (
-        <div className='my-4 mx-4'>
-        <Tabs
-          defaultActiveKey="users"
-          id="admin-main-tab"
-          className="mb-3"
-        >
-          <Tab eventKey="users" title="Users">
-            {UserTab()}
-          </Tab>
-          <Tab eventKey="scripts" title="Scripts">
-            {ScriptTab()}
-          </Tab>
-          {/* <Tab eventKey="contact" title="Contact" disabled>
-            #3
-          </Tab> */}
-        </Tabs>
-        </div>
-      );
-}
+const { Header, Content, Footer, Sider } = Layout;
 
-export default AdminDashboard
+const items = [{
+ key:'users',
+ icon: React.createElement(UserOutlined),
+ label:'Users' 
+},
+{
+  key:'scripts',
+  icon: React.createElement(BarChartOutlined),
+  label:'Scripts' 
+ },
+ {
+  key:'statistic',
+  icon: React.createElement(UploadOutlined),
+  label:'Statistic' 
+ }]
+
+const AdminDashboard = () => {
+  const [key, setKey] = useState('users')
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div className="demo-logo-vertical">Herme Admin</div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['users']} selectedKeys={[key]} items={items} onClick={(e)=>{
+          console.log('e=====>', e)
+          setKey(e.key)
+        }}/>
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200, minHeight: '100vh' }}>
+        <Header style={{ padding: 0, background: colorBgContainer}} />
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer}}>
+            {key == 'users' && <UserTab/>}
+            {key == 'scripts' && <ScriptTab/>}
+            {key == 'statistic' && <Statistic />}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Herme Speech ©2023 Created by JHU ECE</Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default AdminDashboard;
