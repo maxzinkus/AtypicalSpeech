82a580b6e25d82bfe69892ce6d8a1fd8888e6b35
Jim <mr.totihuang@gmail.com>
2023-07-22 12:49:50 -0400

    feat: jwt

--- controllers/auth/auth.ctrl.js
+++ controllers/auth/auth.ctrl.js
@@ -1,5 +1,8 @@
 const { AdminUser } = require('../../models')
 const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
+
+const jwtSecret = '35d3331f40dac9c738ab5b9f0ed745adf7a126b218b0dc477d0a006e1914640f423d0e'
 
 exports.register = async (req, res, next) => {
     const { username, password } = req.body
@@ -20,12 +23,27 @@ exports.register = async (req, res, next) => {
       await AdminUser.create({
         username,
         password: hashedPassword,
-      }).then(user =>
+      }).then(user => {
+        const maxAge = 24 * 60 * 60;
+
+        const token = jwt.sign(
+            { id: user._id, username, role: user.role },
+            jwtSecret,
+            {
+              expiresIn: maxAge, // 24hrs in sec
+            }
+        );
+
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: maxAge * 1000, // 3hrs in ms
+        });
+
         res.status(200).json({
           message: "User successfully created",
           user,
         })
-      )
+      })
     } catch (err) {
       res.status(401).json({
         message: "User not successful created",
@@ -51,13 +69,29 @@ exports.login = async (req, res, next) => {
           })
         } else {
             bcrypt.compare(password, user.password).then(function (result) {
-                console.log('=====>', result)
-                result
-                ? res.status(200).json({
+                if(result){
+                    const maxAge = 24 * 60 * 60;
+
+                    const token = jwt.sign(
+                        { id: user._id, username, role: user.role },
+                        jwtSecret,
+                        {
+                          expiresIn: maxAge, // 24hrs in sec
+                        }
+                    );
+    
+                    res.cookie("jwt", token, {
+                        httpOnly: true,
+                        maxAge: maxAge * 1000, // 3hrs in ms
+                    });
+
+                    res.status(200).json({
                     message: "Login successful",
                     user,
-                  })
-                : res.status(400).json({ message: "Login not succesful" })
+                    })
+                }else{
+                    res.status(400).json({ message: "Login not succesful" })
+                }
             })
         }
       } catch (error) {
