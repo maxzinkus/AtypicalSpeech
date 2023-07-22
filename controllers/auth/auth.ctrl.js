33a310d99adc2898dba69e900ce77e30f48a67ce
Jim <mr.totihuang@gmail.com>
2023-07-22 09:12:46 -0400

    feat: crypt password

--- controllers/auth/auth.ctrl.js
+++ controllers/auth/auth.ctrl.js
@@ -1,15 +1,25 @@
 const { AdminUser } = require('../../models')
+const bcrypt = require("bcryptjs")
 
 exports.register = async (req, res, next) => {
     const { username, password } = req.body
+
     if (password.length < 6) {
       return res.status(400).json({ message: "Password less than 6 characters" })
     }
-    console.log(username, password)
+
+    const oldUser = await AdminUser.findOne({ where: { username } });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
     try {
+      const hashedPassword = await bcrypt.hash(password, 10)
+            
       await AdminUser.create({
         username,
-        password,
+        password: hashedPassword,
       }).then(user =>
         res.status(200).json({
           message: "User successfully created",
@@ -33,17 +43,22 @@ exports.login = async (req, res, next) => {
       })
     }
     try {
-        const user = await AdminUser.findOne({ username, password })
+        const user = await AdminUser.findOne({where: { username }})
         if (!user) {
           res.status(401).json({
             message: "Login not successful",
             error: "User not found",
           })
         } else {
-          res.status(200).json({
-            message: "Login successful",
-            user,
-          })
+            bcrypt.compare(password, user.password).then(function (result) {
+                console.log('=====>', result)
+                result
+                ? res.status(200).json({
+                    message: "Login successful",
+                    user,
+                  })
+                : res.status(400).json({ message: "Login not succesful" })
+            })
         }
       } catch (error) {
         res.status(400).json({
