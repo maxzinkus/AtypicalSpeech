e368811519b715464c550b0a7b9aa6c5f0f6c165
Jim <mr.totihuang@gmail.com>
2023-07-28 07:11:48 -0400

    feat: only admin can download audios

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,12 @@
 const { Router } = require('express');
 const router = Router()
-
-router.get('/', (_, res) => {
-    res.send("Welcome to AtypicalSpeech")
-});
+const { adminAuth } = require('../middleware/auth.js');
 
 router.use('/api/script', require('./script'));
 router.use('/api/user', require('./user'));
 router.use('/api/utterancedetail', require('./utterancedetail'));
 router.use('/api/upload', require('./upload'))
-router.use('/api/audios', require('./audios'))
+router.use('/api/audios', adminAuth, require('./audios'))
 router.use('/api/auth', require('./auth'))
 
 module.exports = router;
