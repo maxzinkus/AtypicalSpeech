42803c10104b6ac754a95ddf62692a769faeb58f
Jim <mr.totihuang@gmail.com>
2023-07-28 05:49:30 -0400

    feat: admin auth router

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const ctrl = require('./auth.ctrl');
 
-router.get('/', (_, res) => {
+router.get('/admin', ctrl.adminAuth, (_, res) => {
     res.send("AdminUser")
 });
 
