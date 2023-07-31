a9c4a8f599e78d28593639e31ff67d3159370c5d
Jim <mr.totihuang@gmail.com>
2023-07-31 07:24:06 -0400

    feat: download single audio adjustment

--- controllers/audios/audios.ctrl.js
+++ controllers/audios/audios.ctrl.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const fileType = require('file-type')
 
 const uploadDir = path.resolve(__dirname, '../../uploads');
 
@@ -26,7 +27,21 @@ exports.download_single_audio = async (req, res) => {
     try {
         const { filename } = req.body;
         const file = path.join(uploadDir, filename);
-        res.sendFile(file)
+        
+        fs.readFile(file, async function(err, data) {
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            const base64File = Buffer.from(data).toString('base64');
+            const type = await fileType.fromBuffer(data);
+            
+            res.json({
+                file: base64File,
+                filename: filename,
+                type
+            })
+        });
     } catch (error) {
         return res.status(500).json(error)
     }
