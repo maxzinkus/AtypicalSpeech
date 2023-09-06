bc987070aa5ef3a702d6f952057ea5bf30478a9f
Jim <mr.totihuang@gmail.com>
2023-09-06 18:04:52 -0400

    feat: add a public folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
+const express = require('express');
+const path = require('path');
 const router = Router()
 const { adminAuth } = require('../middleware/auth.js');
+
+// add public static folder
+router.use('/medias', express.static(path.join(__dirname, '../medias')));
 
 router.use('/api/script', require('./script'));
 router.use('/api/user', require('./user'));
