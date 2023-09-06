0e00117ec92c1fe16574d218ad6c0e88ec6e93f6
Jim <mr.totihuang@gmail.com>
2023-09-06 17:58:01 -0400

    get_all_medias

--- controllers/media/index.js
+++ controllers/media/index.js
@@ -25,6 +25,7 @@ const uploadM = multer({ storage: mediaStorage });
 
 router.post('/create', uploadM.single('filename'), ctrl.create_script);
 router.post('/assign', ctrl.assign);
+router.get('/get_all_medias', ctrl.get_all_medias);
 
 
 module.exports = router;
