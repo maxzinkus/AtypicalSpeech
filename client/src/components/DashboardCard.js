2e9f242fddaef6f2e8117c92429d6f7d00f8c22c
Jim <mr.totihuang@gmail.com>
2023-09-06 22:42:24 -0400

    change the desc of card

--- client/src/components/DashboardCard.js
+++ client/src/components/DashboardCard.js
@@ -5,17 +5,15 @@ import { Link, useNavigate } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function DashboardCard({script_id, accessCode}) {
-    console.log("script id: ", script_id)
-    console.log("card access code: ", accessCode)
-
+function DashboardCard({script_id, accessCode, type}) {
     const navigate = useNavigate();
 
     const handleClick = (event) => {
         navigate('/module', {
             state: {
                 accessCode: accessCode,
-                script_id: script_id
+                script_id: script_id,
+                type
             }
         })
     }
@@ -23,10 +21,10 @@ function DashboardCard({script_id, accessCode}) {
   return (
     <div class="card text-center">
     <div class="card-header">
-        Script #{script_id}
+        {`${type?type:'Script'}#${script_id}`}
     </div>
     <div class="card-body">
-        <h3 class="card-title">Script {script_id}</h3>
+        <h3 class="card-title">{`${type?type:'Script'}#${script_id}`}</h3>
         <p class="card-text">Script Text</p>
         <button class="btn btn-primary" onClick={handleClick}>Start</button>
     </div>
